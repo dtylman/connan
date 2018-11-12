c24a689f2dc999ab5be22f6de0ef5bb1d4ebbbd8
danny <dtylman@gmail.com>
2018-11-12 10:02:26 +0200

    fixing ut

--- indexer_test.go
+++ indexer_test.go
@@ -4,15 +4,29 @@ import (
 	"testing"
 	"time"
 
+	"github.com/dtylman/connan/db"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewIndexer(t *testing.T) {
-	i, err := NewIndexer("/tmp/connan.db")
+	db, err := db.Open("/tmp/connan.db")
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer i.Close()
+	assert.NotNil(t, db)
+	defer func() {
+		err := db.Close()
+		assert.NoError(t, err)
+	}()
+	i, err := NewIndexer(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.NotNil(t, i)
+	defer func() {
+		err := i.Close()
+		assert.NoError(t, err)
+	}()
 	err = i.Start("/tmp")
 	assert.Nil(t, err)
 	for i.worker.IsRunning() {
